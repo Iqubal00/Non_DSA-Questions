c5305e42f0686d9c35de8f5e8bb43f9472194cfb
Afsar Iqubal <iqubal7658@gmail.com>
2024-12-02 21:03:43 +0530

    update

--- Array/largestSmallest.java
+++ Array/largestSmallest.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class largestSmallest {
 
@@ -22,9 +23,19 @@ public class largestSmallest {
         return smallest;
     }
 
+    
+   
+    
+
     public static void main(String[] args) {
         int[] arr = {21,4,17,8,29,65};
         System.out.println("Largest number : " + largest(arr));
         System.out.println("Smallest number : " + smallest(arr));
+        System.out.println();
+
+        // or we can just sort the array and print first an last element.
+        Arrays.sort(arr);
+        System.out.println("smallest number : " + arr[0]);
+        System.out.println("largest number : " + arr[arr.length-1]);
     }
 }
